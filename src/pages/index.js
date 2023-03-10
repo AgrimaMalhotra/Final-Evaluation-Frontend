4be41556f10864d2c492a7f29c72cef7fb520267
Agrima <er.agrima.malhotra@gmail.com>
2023-03-10 13:28:55 +0530

    feat: add login & route pages with backend integration

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,3 +2,5 @@ export { default as Error } from './Error';
 export { default as PageNotFound } from './PageNotFound';
 export {default as Dashboard} from './Dashboard';
 export {default as CollectionPage} from './Collections';
+export {default as Login} from './Login';
+export {default as Register} from './Register';
